4e203f3a20f8d0b8dbbfda7a4063541795b37ff2
NatanFrnds <natanemanuelfernandes870@gmail.com>
2025-10-08 22:07:42 -0300

    Dia08/10

--- src/main/java/com/nef/crud_usuarios_java/controller/UserListController.java
+++ src/main/java/com/nef/crud_usuarios_java/controller/UserListController.java
@@ -1,4 +1,45 @@
 package com.nef.crud_usuarios_java.controller;
 
+import com.nef.crud_usuarios_java.Service.UsuarioService;
+import com.nef.crud_usuarios_java.model.Usuario;
+import javafx.beans.Observable;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class UserListController {
+@FXML
+    private TableView<Usuario> tableView;
+
+@FXML
+    private TableColumn<Usuario, String> colName;
+
+    @FXML
+    private TableColumn<Usuario, String> colSobrenome;
+
+    @FXML
+    private TableColumn<Usuario, String> colEmail;
+
+    @FXML
+    private TableColumn<Usuario, String> colTelefone;
+
+    @FXML
+    private TableColumn<Usuario, String> colLogin;
+
+    @FXML
+    private TableColumn<Usuario, String> colAcoes;
+
+    @FXML
+    private Lable statusLable;
+
+    @FXML
+    private Button syncButton;
+
+    private UsuarioService usuarioService;
+    private ObservableList<Usuario>obsUsuario;
+
+
+
 }
