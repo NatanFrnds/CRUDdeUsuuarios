44a048d2f21876abd1a346ccdaa2c4d504776b5b
NatanFrnds <natanemanuelfernandes870@gmail.com>
2025-10-09 19:52:40 -0300

    Atualização do codigo

--- src/main/java/com/nef/crud_usuarios_java/controller/UserFormController.java
+++ src/main/java/com/nef/crud_usuarios_java/controller/UserFormController.java
@@ -1,4 +1,52 @@
 package com.nef.crud_usuarios_java.controller;
 
+import com.nef.crud_usuarios_java.Service.UsuarioService;
+import com.nef.crud_usuarios_java.model.Usuario;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 public class UserFormController {
+    @FXML
+    private Label titleLable;
+    @FXML
+    private TextField nomeField;
+    @FXML
+    private TextField sobrenome;
+    @FXML
+    private TextField emailField;
+    @FXML
+    private TextField loginField;
+    @FXML
+    private TextField telefoneField;
+    @FXML
+    private DatePicker dataNascimentoPicker;
+    @FXML
+    private ChoiceBox<String> sexoChoiceBox;
+    @FXML
+    private TextField enderecoField;
+
+    private Stage stage;
+    private Usuario usuario;
+    private UsuarioService UsuarioService;
+
+    public void initialize(){
+
+    }
+
+    public void setStage(){
+
+    }
+
+    public void setUsuario(){
+
+    }
+
+    @FXML
+public void handleSalvar(){
+
+    }
 }
--- src/main/java/com/nef/crud_usuarios_java/controller/UserListController.java
+++ src/main/java/com/nef/crud_usuarios_java/controller/UserListController.java
@@ -2,18 +2,19 @@ package com.nef.crud_usuarios_java.controller;
 
 import com.nef.crud_usuarios_java.Service.UsuarioService;
 import com.nef.crud_usuarios_java.model.Usuario;
-import javafx.beans.Observable;
+
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.Label;
 
 public class UserListController {
-@FXML
+    @FXML
     private TableView<Usuario> tableView;
 
-@FXML
+    @FXML
     private TableColumn<Usuario, String> colName;
 
     @FXML
@@ -32,14 +33,35 @@ public class UserListController {
     private TableColumn<Usuario, String> colAcoes;
 
     @FXML
-    private Lable statusLable;
+    private Label statusLable;
 
     @FXML
     private Button syncButton;
 
     private UsuarioService usuarioService;
-    private ObservableList<Usuario>obsUsuario;
+    private ObservableList<Usuario> obsUsuario;
 
+    public void initialize() {
+    }
 
+    public void atualizarStatusConexao() {
+    }
+
+    @FXML
+    public void handleSincronizar() {
+    }
+
+    public void carregarDadosTable() {
+    }
+
+    public void adicionarBotoesDeAcao() {
+    }
+
+    @FXML
+    public void handleAdicionarUsuario() {
+    }
+
+    public void abrirFormularioUsuario() {
+    }
 
 }
